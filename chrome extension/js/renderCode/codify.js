d8bd65d7faadc41c6769a2dc9251f83912fca835
PabiGamito <pablogamito@gmail.com>
2018-02-09 00:12:16 +0000

    added node packages

--- chrome extension/js/renderCode/codify.js
+++ chrome extension/js/renderCode/codify.js
@@ -1,3 +1,6 @@
+// NOTE: Always bundle this code with: browserify codify.js -o bundle.js 
+var lang = require('language-classifier');
+
 $(document).ready(function() {
   var msgCount = 0
   var codeRegexPattern = /\[([a-z]*)\]((.|\n)*)\[\/([a-z]*)\]/g;
@@ -10,23 +13,47 @@ $(document).ready(function() {
       var messengerText = messengerBubble.firstChild.innerHTML
       var match = codeRegexPattern.exec(messengerText);
       if (match != null) {
-        messengerBubble.parentNode.style.padding = "0";
-        messengerBubble.parentNode.style.borderRadius = "25px";
-        messengerBubble.innerHTML = '';
         var language = match[1];
         var codeStr = match[2];
-
-        var pre = document.createElement('pre');
-        pre.className = "prettyprint prettyprinted";
-        pre.style.padding = "10px";
-        pre.style.borderRadius = "25px";
-        var formattedCode = PR.prettyPrintOne(codeStr, language, true);
-        pre.innerHTML = formattedCode;
-
-        messengerBubble.appendChild(pre);
+        formatCode(codeStr,language);
+      } else if (text.includes("{") || 
+          text.includes(";\n") || 
+          text.includes("()") || 
+          text.includes("//") ||
+          text.includes("/*") ||
+          text.includes("&&") ||
+          text.includes("||") ||
+          text.includes("!==") ||
+          text.includes("==") ||
+          text.includes(":")) {
+        // Semi-colons at the end of a line. This alone would catch a whole bunch of languages.
+        // Parentheses directly following text with no space to separate it: myFunc()
+        // A dot or arrow between two words: foo.bar = ptr->val
+        // Presence of curly braces, brackets: while (true) { bar[i]; }
+        // Presence of "comment" syntax (/*, //, etc):
+        // Uncommon characters/operators: +, *, &, &&, |, ||, <, >, ==, !=, >=, <=, >>, <<, ::, __
+        // Run your syntax highlighter on the text. If it ends up highlighting some high percentage of it, it's probably code.
+        // camelCase text in the post.
+        // nested parentheses, braces, and/or brackets.
+        language = lang(text);
+        formatCode(messengerText,language);
       }
     }
     msgCount = messengerBubbles.length;
+  }
+
+  function formatCode(codeStr, language) {
+    messengerBubble.parentNode.style.padding = "0";
+    messengerBubble.parentNode.style.borderRadius = "25px";
+    messengerBubble.innerHTML = '';
+    var pre = document.createElement('pre');
+    pre.className = "prettyprint prettyprinted";
+    pre.style.padding = "10px";
+    pre.style.borderRadius = "25px";
+    var formattedCode = PR.prettyPrintOne(codeStr, language, true);
+    pre.innerHTML = formattedCode;
+
+    messengerBubble.appendChild(pre);
   }
 
   replaceCode();
